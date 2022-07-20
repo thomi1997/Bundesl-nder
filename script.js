3c7bc6595585b4668a9667856466788f6705aae5
unknown <thomas_ketler@web.de>
2022-07-20 19:18:52 +0200

    bundesländer

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+let bundeslaender = [];
+
+
 async function getBundesLeander() {
-    let response = await fetch("/.Bundesland.json");
-    let responseAsJson = await response.json();
-    console.log('Bundesländer', responseAsJson);     
+    let response = await fetch('/.bundesland.json');
+    bundeslaender = await response.json();
+    render();   
+}
+
+
+function render() {
+    let content = document.getElementById('states');
+    content.innerHTML = '';
+
+        for (let i = 0; i < bundeslaender.length; i++) {
+            const state = bundeslaender[i]['name'];
+            content.innerHTML += /*html*/`
+            <div>${state['name']}</div>
+            `;
+        }
 }
