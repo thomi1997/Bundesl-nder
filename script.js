133c8aef8187fe2d7170a4ac45f1ada76018760c
unknown <thomas_ketler@web.de>
2022-07-23 13:52:43 +0200

    bundesländer finish

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let bundeslaender = [];
+let letters = [];
 
 
 async function getBundesLeander() {
@@ -9,23 +10,56 @@ async function getBundesLeander() {
 }
 
 
-function render() {
+function render(filter) {
     let content = document.getElementById('states');
     content.innerHTML = '';
 
         for (let i = 0; i < bundeslaender.length; i++) {
             const state = bundeslaender[i]['name'];
             const url = bundeslaender[i]['url'];
-            const population = bundeslaender[i]['population']; 
-            content.innerHTML += /*html*/`
-            <a class="state-link state" href="${url}">
-                <span>
-                    ${state}
-                </span>
-                <span>
-                    ${population} Millionen
-                </span>
-            </a>
-            `;
+            const population = (bundeslaender[i]['population'] + '').replace('.', ',');
+            const firstLetter = state.charAt(0);
+
+            if (!filter || filter == firstLetter) {
+            content.innerHTML += htmlrender(state, url, population);}
+
+            if (!letters.includes(firstLetter)) {
+                letters.push(firstLetter);
+            }
         }
+        renderLettersList();
+}
+
+
+function htmlrender(state, url, population) {
+    return /*html*/`
+    <a class="state-link state" href="${url}" target="_blank">
+        <div class="font-weight-bold">
+            ${state}
+        </div>
+        <div class="population-color">
+            ${population} Millionen
+        </div>
+    </a>
+    `;
+}
+
+
+function setFilter(letter) {
+    render(letter);
+}
+
+
+function renderLettersList() {
+    let addLetters = document.getElementById('add-letter');
+    addLetters.innerHTML = '';
+
+    for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        addLetters.innerHTML += /*html*/`
+        <div onclick="setFilter('${letter}')" class="search-button">
+            ${letter}
+        </div>
+        `;
+    }
 }
